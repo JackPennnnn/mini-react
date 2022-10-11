b391d37de49ffa46593de9522e3d4eaeaf2a39a3
JackPennnnn <33740709+JackPennnnn@users.noreply.github.com>
2022-10-11 15:06:05 +0800

    React函数式组件实现

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from './packages/core/React';
 import ReactDOM from './packages/core/react-dom';
 
 // jsx
-let element = <h1 style={{color:'red'}}>Hello World</h1>
+// let element = <h1 style={{color:'red'}}>Hello World</h1>
 // babel => js方法 React.createElement() => vNode
 let element2 = React.createElement('h1',{
     style:{
@@ -10,7 +10,20 @@ let element2 = React.createElement('h1',{
     }
 },'Hello World',React.createElement('span', {},66))
 
+// 函数式组件？ 就是一个函数
+// 特点：
+// 1 函数组件的名称首字母大写 react原生组件 div span 自定组件大写
+// 2 函数式组件的返回值 => jsx
+// 3 jsx是一个父子结构（只有一个根）
+// 4 还有一个属性props
+
+function Test(props){
+    return <h1>
+        <div>Test 23 {props.name}</div>
+    </h1>
+}
+
 // 把vNode => 真实dom 放到指定的位置
-ReactDOM.render(element2,
+ReactDOM.render(<Test name={'123456'}></Test>,
     document.getElementById('root'));
 
--- src/packages/core/react-dom.js
+++ src/packages/core/react-dom.js
@@ -3,10 +3,10 @@
 import {REACT_TEXT} from "../constants";
 
 function render(vdom, container) {
-    mount(vdom,container)
+    mount(vdom, container)
 }
 
-function mount(vdom,container){
+function mount(vdom, container) {
     // 1 vdom => 真实dom
     let newDom = createDom(vdom)
     // 2 真实dom放到对应位置
@@ -17,19 +17,27 @@ function createDom(vdom) {
     // vdom => 真实dom
     let {type, props} = vdom
     let dom
-    // 1. 判断type => 文本或者元素
-    if(type === REACT_TEXT){
-        dom = document.createTextNode(vdom.content)
-    }else{
-        dom = document.createElement(type)
-    }
-    // 2. 处理属性
-    if(props){
-        updateProps(dom,{},props) // 真实dom，旧的属性，新的属性
 
+    if(type){
+        // 1. 判断type => 文本或者元素
+        if (type === REACT_TEXT) {
+            dom = document.createTextNode(vdom.content)
+        } else if (typeof type === 'function') {
+            return mountFunctionComponent(vdom)
+        } else {
+            dom = document.createElement(type)
+        }
+    }else{
+    //    2. 没有就当成文本
+        dom = document.createTextNode(vdom)
+    }
+
+    // 2. 处理属性
+    if (props) {
+        updateProps(dom, {}, props) // 真实dom，旧的属性，新的属性
         let children = props.children
-        if(children){
-            changeChildren(children,dom)
+        if (children) {
+            changeChildren(children, dom)
         }
     }
 
@@ -37,27 +45,27 @@ function createDom(vdom) {
 }
 
 // 更新属性
-function updateProps(dom,oldProps,newProps){
-    for(let key in newProps){
+function updateProps(dom, oldProps, newProps) {
+    for (let key in newProps) {
         // 注意children
-        if(key === 'children'){
+        if (key === 'children') {
             continue
-        }else if(key === 'style'){
+        } else if (key === 'style') {
             // 遍历style
             let styleObject = newProps[key]
-            for(let arr in styleObject){
-                dom.style[arr] =styleObject[arr]
+            for (let arr in styleObject) {
+                dom.style[arr] = styleObject[arr]
             }
-        }else { //其他属性
+        } else { //其他属性
             dom[key] = newProps[key]
         }
     }
 
     // 更新处理
-    if(oldProps){
+    if (oldProps) {
         // 旧的属性在新的属性中没有 =》 删除
-        for(let key in oldProps){
-            if(!newProps[key]){
+        for (let key in oldProps) {
+            if (!newProps[key]) {
                 dom[key] = null
             }
         }
@@ -65,14 +73,21 @@ function updateProps(dom,oldProps,newProps){
 }
 
 // 处理children
-function changeChildren(children,dom){
+function changeChildren(children, dom) {
     // 1. 有一个child
-    if(typeof children === 'object' && children.type){
-        mount(children,dom)
-    }else if(Array.isArray(children)){ // 2. 有多个children []
-        children.forEach(item => render(item,dom))
+    if (typeof children === 'object' && children.type) {
+        mount(children, dom)
+    } else if (Array.isArray(children)) { // 2. 有多个children []
+        children.forEach(item => render(item, dom))
     }
 
+}
+
+// 处理函数式组件
+function mountFunctionComponent(vdom) {
+    let {type, props} = vdom
+    let functionVdom = type(props)
+    return createDom(functionVdom)
 }
 
 const ReactDom = {
